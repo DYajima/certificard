f993ae1fcc163aaf15ade2b8330601d405ff90c6
DYajima <101752432+DYajima@users.noreply.github.com>
2022-03-21 12:04:08 -0300

    Adicionei os arquivos do certificard

---
+++ script.js
@@ -0,0 +1,13 @@
+// FIGMA AULA 9
+// https://www.figma.com/file/1flmz2iauuNs8JsY6eaBHI/Imers%C3%A3o-Dev-Aula-9?node-id=0%3A1
+
+//FIGMA AULA 10
+//https://www.figma.com/file/I4p9ndfOKgjDKalFwd5ASr/Imers%C3%A3o-Dev---Aula-10?node-id=0%3A1
+
+// DESAFIOS AULA9
+//Terminar todo o design que está no figma (disponível abaixo)
+//Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
